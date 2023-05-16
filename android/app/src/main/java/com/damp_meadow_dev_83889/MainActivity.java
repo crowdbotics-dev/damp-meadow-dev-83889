422568a2d9db3f6d99aa43a90ad995d73a8a2c31
Crowdbotics <engineering@crowdbotics.com>
2023-05-16 23:18:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/damp_meadow_dev_83889/MainActivity.java
@@ -0,0 +1,15 @@
+package com.damp_meadow_dev_83889;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "damp_meadow_dev_83889";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-meadow-dev-83889",
+  url: "https://damp-meadow-dev-83889.botics.co",
+  api: "https://damp-meadow-dev-83889.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
